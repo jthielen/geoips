fa9ae23bd4922963e9559b24685ecd064dc7c5ad
JT Thielen <github@thielen.science>
2026-09-08 15:54:39 -0600

    IN PROGRESS: rework for support file creation script

--- docs/create_support_files.py
+++ docs/create_support_files.py
@@ -1,22 +1,31 @@
 # # # This source code is subject to the license referenced at
 # # # https://github.com/NRLMMD-GEOIPS.
 
-"""Create the pydata-sphinx-theme version switcher json file.
+"""Create the support files for versioned docs.
 
-TODO: rework to be compatabile with both old and new approaches
+This includes:
+- the `versions.json` file which drives the `pydata-sphinx-theme`'s version switcher
+- the symlink `stable` to the latest minor release family
+- a root-level `index.html` that redirects to `/stable`
+- a root-level `404.html` that redirects URLs without a version string to the matching
+  path on `stable`
 """
 
 import argparse
 import json
 from pathlib import Path
-from typing import List, Dict
+import re
+from typing import List, Dict, Union
+
+from packaging import Version
+
 
 DEFAULT_BASE_URL = "https://nrlmmd-geoips.github.io/geoips/"
 BASE_VERSION_CONFIG = [
     {
         "version": "stable",
         "url": f"{BASE_URL}stable/",
-        "is_latest": true
+        "is_latest": True
     },
     {
         "version": "dev",
@@ -24,8 +33,8 @@ BASE_VERSION_CONFIG = [
     },
 ]
 
+"""
 def get_version_list(filepath: Path) -> List[Dict]:
-    """Open json file and return list if it passes validation."""
     with filepath.open() as f:
         output = json.load(f)
     if (
@@ -39,7 +48,6 @@ def get_version_list(filepath: Path) -> List[Dict]:
 
 
 def update_version_list(filepath: Path, name: str, current_list: List):
-    """Update version json, inserting as new 3rd element behind stable and dev."""
     this_version_dict = {"url": f"{BASE_URL}{name}/"}
     if name.startswith("v"):
         this_version_dict["name"] = name
@@ -49,31 +57,121 @@ def update_version_list(filepath: Path, name: str, current_list: List):
     new_version_list = current_list.append(this_version_dict)
     with filepath.open("w") as f:
         json.dump(new_version_list, f)
+"""
+
+version_dir_pattern = re.compile("^v[0-9]")
+
+
+def scan_for_directories(scan_path: Path) -> List[str]:
+    # TODO docstring
+    all_subdirs = [
+        str(p.relative_to(scan_path)) for p in scan_path.iterdir() if p.is_dir()
+    ]
+    return [p for p in all_subdirs if version_dir_pattern.search(p)]
+
+
+def create_version_config(versions: List[str], base_url: str) -> List[Dict[str, Union[str, bool]]]:
+    # TODO docstring
+    # Initial config, always include stable and dev
+    config = [
+        {
+            "version": "stable",
+            "url": f"{base_url}stable/",
+            "is_latest": True
+        },
+        {
+            "version": "dev",
+            "url": f"{base_url}dev/"
+        },
+    ]
+    # Now, add the directories, sorted by version
+    for version_dir in sorted(versions, lambda v: Version(v[1:])):
+        config.append({
+            "name": version_dir,
+            "version": version_dir[1:],
+            "url": f"{base_url}{version_dir}/"
+        })
+    return config
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Conditionally update pydata-sphinx-theme version switcher json"
+        description="Create support files for versioned docs"
     )
 
+    """
+    TODO:
+    - args
+        - `--create-version-file`
+        - `--create-stable-symlink`
+        - `--create-index-redirect`
+        - `--create-404-redirect`
+        - `--scan-dir`
+        - `--output-dir`
+        - `--base-url`
+    """
     parser.add_argument(
-        "-n",
-        "--name",
-        type=str,
-        help="Name of version"
+        "-S",
+        "--scan-dir",
+        type=Path,
+        help=(
+            "Path containing version directories to parse (required for "
+            "--create-version-file or --create-stable-symlink)"
+        ),
+        default=argparse.SUPPRESS
     )
     parser.add_argument(
-        "-f",
-        "--file",
+        "-O",
+        "--output-dir",
         type=Path,
-        help="Path to json version file"
+        help="Output Path",
+        required=True
+    )
+    parser.add_argument(
+        "--base-url",
+        type=str,
+        help="Base URL to which GitHub Pages for this package deploys to",
+        default=DEFAULT_BASE_URL
+    )
+    parser.add_argument(
+        "--create-version-file",
+        action="store_true",
+        help="Create versions.json file in output dir"
+    )
+    parser.add_argument(
+        "--create-stable-symlink",
+        action="store_true",
+        help="Create symlink `stable` to latest (non-dev) directory"
+    )
+    parser.add_argument(
+        "--create-index-redirect",
+        action="store_true",
+        help="Create an index.html containing a meta-redirect to `stable`"
+    )
+    parser.add_argument(
+        "--create-404-redirect",
+        action="store_true",
+        help=(
+            "Create a 404.html containing JS redirect of non-versioned paths to the "
+            "equivalent on `stable`"
+        )
     )
 
     args = parser.parse_args()
-    version_list = get_version_list(args.file)
-    versions_present = [item["version"] for item in version_list]
-    if args.name not in versions_present:
-        update_version_list(args.file, args.name, version_list)
-        print(f"Version {args.name} added to version json.")
-    else:
-        print(f"Version {args.name} already present in version json, no need to update.")
+
+    # Validate inputs
+    if (
+        (args.create_version_file or args.create_stable_symlink)
+        and not hasattr(args, "scan_dir")
+    ):
+        raise ValueError((
+            "Version file and/or stable symlink cannot be created without an input "
+            "--scan-dir"
+        ))
+    if not args.base_url.endswith("/"):
+        raise ValueError("--base-url must have a trailing slash to be valid")
+
+    version_dirs = (
+        scan_for_directories(args.scan_dir) if hasattr(args, "scan_dir") else None
+    )
+
